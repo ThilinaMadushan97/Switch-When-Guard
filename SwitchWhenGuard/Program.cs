f3d80774e48cbae917b0e36a9b7adad61c400622
dell <you@example.com>
2024-04-12 21:39:09 +0530

    Add project files.

---
+++ SwitchWhenGuard/Program.cs
@@ -0,0 +1,22 @@
+﻿var peoples = new List<Person>
+{
+    new ("Thilina" ,"Madushan", 16),
+    new ("Saman" ,"Kumara", 56),
+    new ("Asela" ,"Gunarathne", 17),
+    new ("Sanath" ,"Jayasooriya", 60),
+
+};
+string CheckPerson(Person person)
+{
+    return person switch
+    {
+        {Age: var age} when age > 18 => "Adult person",
+        _ => "Minor person"
+    };
+}
+
+foreach (var p in peoples)
+{
+    Console.WriteLine(CheckPerson(p));
+}
+record Person(string Fname, string Lname, int Age);
